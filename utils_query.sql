3ac1c98d7204ab6f3c50c37d0db298f30e870c93
Michele <Salvatori>
2020-02-15 18:33:55 +0100

    update

--- utils_query.sql
+++ utils_query.sql
@@ -1,20 +1,8 @@
 INSERT INTO auction_house.utenti VALUES
 ("SLVMHL98T07A123M", "Piero", "Salvatori", "1998-12-07", "Alatri", "via vicinale cesadoni 28", "03010", "5410640744377561", "2024-01-01", "Michele", "Salvatori", "981", "pippo");
 
-delete from auction_house.utenti where CF_Utente = "SLVCHL98T07A123M";
-
-ALTER TABLE auction_house.utenti modify numero_carta varchar(16);
-
-select * from auction_house.utenti ;
-
-update auction_house.utenti set numero_carta = "5410640744377561" where CF_Utente = "SLVMHL98T07A123M";
-
 INSERT INTO auction_house.offerte values
-("SLVMHL98T07A123M", current_timestamp, "100", "15");
-
-select * from auction_house.offerte;
-select * from auction_house.categoria;
-select * from auction_house.catIndex;
+("SLVMHL98T07A123M", current_timestamp, "100", null);
 
 INSERT INTO auction_house.categoria VALUES
 ("Piccoli", "3");
@@ -22,3 +10,21 @@ INSERT INTO auction_house.categoria VALUES
 INSERT INTO auction_house.catIndex VALUES
 ("Cucina", "Grandi");
 
+
+delete from auction_house.utenti where CF_Utente = "SLVMHL98T07A123M";
+delete from auction_house.offerte where CF_Utente = "SLVMHL98T07A123M" and datatime_stamp <= now();
+
+ALTER TABLE auction_house.categoria ADD  PRIMARY KEY;
+ALTER TABLE auction_house.offerte drop foreign key offerte_ibfk_1;
+ALTER TABLE auction_house.offerte ADD CONSTRAINT offerte_ibfk_1 FOREIGN KEY (CF_Utente) REFERENCES auction_house.utenti(CF_Utente) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+
+select * from auction_house.offerte;
+select * from auction_house.categoria;
+select * from auction_house.catIndex;
+select * from auction_house.utenti;
+
+
+
+insert into auction_house.prova values(CURDATE(), now);
